31d70cc78136e5533ec676ecd5496889d828e38e
Geovani Gabriel <cambuenga1998@gmail.com>
2023-08-20 16:41:56 +0100

    Nome e Sobrenome

--- BOX/settings.py
+++ BOX/settings.py
@@ -140,4 +140,13 @@ MEDIA_URL = '/media/'
 DATE_INPUT_FORMAT = ["%d/%m/%Y"]
 PHONENUMBER_DEFAULT_REGION = 'AO'
 
+##################################
+
+EMAIL_HOSTS = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'myemail@gmail.com'
+EMAIL_HOST_PASSWORD = '*****'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
